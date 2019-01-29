5318bcad47b98079bf0542b6c59827d2db731125
Liam Lian <liam.lian@smith.co>
2019-01-29 19:09:56 +0800

    update gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,7 +1,22 @@
 
 var gulp =  require('gulp');
 var less = require('gulp-less');
-gulp.task('default', function() {
+var rename = require('gulp-rename');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
+
+gulp.task('default', function() {
+    return  gulp.src('../src/**/style.less')
+    .pipe(less({
+        plugins : [autoprefix]
+    }))
+    .pipe(rename(function(path) {
+        var dirname = path.dirname;
+        console.log(path.dirname);
+        path.dirname  = dirname.replace(new RegExp("themes\\(.*)"),"themes-$1");
+        console.log(path.dirname);
+    }))
+    .pipe(gulp.dest('../_ui/'))
 
 })
