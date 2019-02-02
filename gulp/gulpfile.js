5ca2ff200c800f579c2551ccdf2f988dd04eb344
Liam Lian <liam.lian@smith.co>
2019-02-02 19:26:05 +0800

    update gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,22 +2,52 @@
 var gulp =  require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
-var clean = require('gulp-clean');
+var del = require('del');
 var lesshint = require('gulp-lesshint');
 var jshint = require('gulp-jshint');
-var sourcemap = require('gulp-sourcemaps');
+var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
 var LessAutoprefix = require('less-plugin-autoprefix');
 var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
-gulp.task('clean-folder', function() {
-    return gulp.src('../_ui/**')
-            .pipe(clean({force: true}));
-});
+// function list
 
-gulp.task('less', function() {
-    return gulp.src('../src/**/style.less')
+// Clean the _ui folder.
+gulp.task('clean', function() {
+    return del([
+        '../_ui/**'
+    ], {force: true}).then(function() {
+        console.log('Clean the _ui folder...');
+    })
+});
+// Copy the fonts to the ui folder.
+gulp.task('copy', function() {
+    return gulp.src('../src/**/fonts/**')
+    .pipe(rename(function(path) {
+        path.dirname  = path.dirname.replace(/themes\\(.*)/,"themes-$1");
+    }))
+    .pipe(gulp.dest('../_ui/'))
+})
+// Compiling less 
+gulp.task('less:dev', function() {
+    return gulp.src('../src/**/**/style.less')
+            .pipe(lesshint({
+                configPath: '.lesshintrc'
+                // Options TODO
+            }))
+            .pipe(less({
+                plugins : [autoprefix]
+            }))
+            .pipe(rename(function(path) {
+                path.dirname  = path.dirname.replace(/themes\\(.*)\\less/,"themes-$1\\css");
+            }))
+            .pipe(gulp.dest('../_ui/'));
+});
+// Compiling and minify less
+gulp.task('less:pro', function() {
+    return gulp.src('../src/**/**/style.less')
             .pipe(lesshint({
                 configPath: '.lesshintrc'
                 // Options
@@ -26,12 +56,29 @@ gulp.task('less', function() {
                 plugins : [autoprefix]
             }))
             .pipe(rename(function(path) {
+                path.dirname  = path.dirname.replace(/themes\\(.*)\\less/,"themes-$1\\css");
+                path.basename += '.min';
+            }))
+            .pipe(cleanCSS())
+            .pipe(gulp.dest('../_ui/'))
+});
+// Compression customer js and create map
+gulp.task('js:dev', function() {
+    return gulp.src('../src/**/**.js')
+            .pipe(jshint())
+            .pipe(sourcemaps.init())
+            .pipe(concat('main.js'))
+            .pipe(rename(function(path) {
                 path.dirname  = path.dirname.replace(/themes\\(.*)/,"themes-$1");
             }))
-            .pipe(gulp.dest('../_ui/'));
+            .pipe(gulp.dest('../_ui/js'))
+            .pipe(uglify())
+            .pipe(rename('main.min.js'))
+            .pipe(sourcemaps.write('maps'))
+            .pipe(gulp.dest('../_ui/js'));
 });
-
-gulp.task('javascript', function() {
+// Compression customer js only
+gulp.task('js:pro', function() {
     return gulp.src('../src/**/**.js')
             .pipe(jshint())
             .pipe(concat('main.js'))
@@ -39,11 +86,34 @@ gulp.task('javascript', function() {
                 path.dirname  = path.dirname.replace(/themes\\(.*)/,"themes-$1");
             }))
             .pipe(uglify())
-            .pipe(gulp.dest('../_ui/'));
+            .pipe(rename('main.min.js'))
+            .pipe(gulp.dest('../_ui/js'));
 });
-
+// Compression plugins js only
+gulp.task('js:plugins', function() {
+    return gulp.src('../src/**/**.js')
+            .pipe(jshint())
+            .pipe(concat('plugins.js'))
+            .pipe(rename(function(path) {
+                path.dirname  = path.dirname.replace(/themes\\(.*)/,"themes-$1");
+                path.basename += '.min';
+            }))
+            .pipe(uglify())
+            .pipe(rename('main.min.js'))
+            .pipe(gulp.dest('../_ui/js'));
+});
+// Watch TODO
+gulp.task('watch', function() {
+    gulp.watch('../src/themes/**/less/*.less',  gulp.series('less'));
+    gulp.watch('../src/**/*.js',  gulp.series('js:dev'));
+});
 gulp.task('default', 
-    gulp.series('clean-folder', 
-        gulp.parallel('less', 'javascript')
+    gulp.series('clean', 
+        gulp.parallel('copy', 'less:pro', 'js:pro')
+    )
+);
+gulp.task('dev', 
+    gulp.series('clean', 
+        gulp.parallel('copy', 'less:dev', 'js:dev')
     )
 );
